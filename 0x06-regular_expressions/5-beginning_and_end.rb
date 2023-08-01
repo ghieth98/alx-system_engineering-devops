32644d7d3a7b499e24f9fc474e46f7169ed49158
ghieth98 <hossamghieth@gmail.com>
2023-08-01 13:06:00 -0700

    fix beginning and end

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Script that accepts one arugment and pass it to  a regular expression matching method
-puts ARGV[0].scan[/h.n/).join
+# Script that accepts one arugment and pass it to a regular expression matching method
+puts ARGV[0].scan(/h.n/).join
