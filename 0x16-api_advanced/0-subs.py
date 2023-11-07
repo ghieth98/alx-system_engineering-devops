40b614ea9ad8d7f632be2530a8b9d78d24b43f7b
ghieth98 <hossamghieth@gmail.com>
2023-11-07 09:03:32 -0800

    fixed task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-0-subs.py
+0-sub.py
 """
 import requests
 
 
-def number_od_subscribers(subreddit):
+def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers (not active users, total subscribers)
     for a given subreddit
